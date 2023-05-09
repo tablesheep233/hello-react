eef6274bf44df09ad6bffa2bb31033ee2a91e5c5
zhuoyang <858916094@qq.com>
2023-05-09 16:33:00 +0800

    favorites

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom"
-import classes from "./MainNavigation.module.css"
-
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
 
 export default function MainNavigation() {
-    return <header className={classes.header}>
-        <div className={classes.logo}>React Meetups</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">All Meetups</Link>
-                </li>
-                <li>
-                    <Link to="/new-meetup">New Meetup</Link>
-                </li>
-                <li>
-                    <Link to="/favorites">My Favorites</Link>
-                </li>
-            </ul>
-        </nav>
+  const favoritesCtx = useContext(FavoritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">My Favorites</Link>
+            <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+          </li>
+        </ul>
+      </nav>
     </header>
+  );
 }
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,3 +1,22 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
+
 export default function FavoritesPage() {
-    return <div><h1>Favorites Page</h1></div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <div>
+      <h1>Favorites Page</h1>
+      {content}
+    </div>
+  );
 }
